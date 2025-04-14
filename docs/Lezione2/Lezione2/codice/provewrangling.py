e156eef93494eed740469c6d2cc93d5b972d0da5
andbar27 <andrea.barbato2796@icloud.com>
2025-04-14 16:47:44 +0200

    min

--- docs/Lezione2/Lezione2/codice/provewrangling.py
+++ docs/Lezione2/Lezione2/codice/provewrangling.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 
+from sqlalchemy import create_engine, text
+
 filename = 'https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data'
 
 
@@ -10,3 +12,43 @@ headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num
           "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
           "peak-rpm","city-mpg","highway-mpg","price"]
 
+dict =     {
+
+        "Name": [
+
+            "Braund, Mr. Owen Harris",
+
+            "Allen, Mr. William Henry",
+
+            "Bonnell, Miss. Elizabeth",
+
+            "Taylor, Miss. Jane"
+
+        ],
+
+        "Age": [22, 35, 58, 55],
+
+        "Sex": ["male", "male", "female", "female"],
+
+        "Location": ["Rome", "London", "Berlin", "New York"],
+
+    }
+ 
+df = pd.DataFrame(dict)
+print("Un dataframe creato da un dizionario Python")
+print(df)
+print("\n")
+
+
+# STORE DATA IN PostgreSQL DB
+#Set up database connection (modify with your credentials)
+engine = create_engine('postgresql+psycopg://postgres:postgres@postgresql:5432/prova_db')
+
+# Prepare una tabella
+passenger_info = df[['Name', 'Sex', 'Age', 'Location']]
+
+# Store in PostgreSQL
+passenger_info.to_sql('passenger_info', engine, if_exists='replace', index=False)
+
+print("\nData stored in PostgreSQL database")
+print("\n")
